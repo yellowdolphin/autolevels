1dd80108d8f76f7dee3981cc75a0be509fb99372
yellowdolphin <marius.wanko@gmail.com>
2023-02-05 02:04:01 +0100

    improve black/whitepoint shifts in low-contrast images
    - rename args constant-blackshift/constant-whiteshift ->
      max-blackshift/max-whiteshift
    - scale blackshift down to zero for blackpoint=255
    - scale whiteshift down to zero for whitepoint=0

--- autolevels.py
+++ autolevels.py
@@ -24,10 +24,10 @@ parser.add_argument('--maxblack', nargs='+', default=75, type=int,
                                   help="Max allowed RGB value(s) for full blackpoint correction")
 parser.add_argument('--minwhite', nargs='+', default=240, type=int,
                                   help="Min allowed RGB value(s) for full whitepoint correction")
-parser.add_argument('--constant-blackshift', nargs='+', default=[27, 22, 28], type=int,
-                                             help="blackpoint shift if MAXBLACK is exceeded")
-parser.add_argument('--constant-whiteshift', nargs='+', default=0, type=int,
-                                             help="whitepoint shift if MINWHITE is not achieved")
+parser.add_argument('--max-blackshift', nargs='+', default=[27, 22, 28], type=int,
+                                        help="max blackpoint shift if MAXBLACK is exceeded")
+parser.add_argument('--max-whiteshift', nargs='+', default=0, type=int,
+                                        help="max whitepoint shift if MINWHITE is not achieved")
 parser.add_argument('--mode', default='hist', choices=['smooth', 'smoother', 'hist', 'perceptive'], 
                               help='Blackpoint sample mode: "smooth", "smoother", "hist", or "perceptive"')
 parser.add_argument('--gamma', nargs='+', type=float, default=[1.0], 
@@ -49,10 +49,10 @@ arg = parser.parse_args()
 
 black_pixel = np.array(arg.blackpixel, dtype=float)
 max_black = np.array(arg.maxblack, dtype=int)
-constant_black = np.array(arg.constant_blackshift, dtype=int)
+max_blackshift = np.array(arg.max_blackshift, dtype=int)
 white_pixel = np.array(arg.whitepixel, dtype=float)
 min_white = np.array(arg.minwhite, dtype=int)
-constant_white = np.array(arg.constant_whiteshift, dtype=int)
+max_whiteshift = np.array(arg.max_whiteshift, dtype=int)
 sample_mode = arg.mode
 assert all(g > 0 for g in arg.gamma), f'invalid gamma {arg.gamma}, must be positive'
 gamma = 1 / np.array(arg.gamma, dtype=float)
@@ -132,8 +132,9 @@ def get_blackpoint_whitepoint(img, mode, pixel_black, pixel_white):
                     break
             whitepoint.append(x)
 
-        for i, t in enumerate(ts):
-            print(f"timer {i}: {t}")
+        #for i, t in enumerate(ts):
+        #    print(f"timer {i}: {t}")
+
         return np.array(blackpoint), np.array(whitepoint)
 
     elif mode.startswith('hist'):
@@ -195,9 +196,11 @@ for fn in fns:
     target_black = np.array(arg.blackpoint, dtype=int)
     target_white = np.array(arg.whitepoint, dtype=int) if arg.whitepoint else None
     if (blackpoint > max_black).any():
-        target_black = np.maximum(target_black, blackpoint - constant_black)
+        shift = max_blackshift * (255 - blackpoint) / (255 - max_blackshift)
+        target_black = np.maximum(target_black, blackpoint - shift)
     if (whitepoint < min_white).any() and arg.whitepoint:
-        target_white = np.minimum(target_white, whitepoint - constant_white)
+        shift = max_whiteshift * whitepoint / (255 - max_whiteshift)
+        target_white = np.minimum(target_white, whitepoint + shift)
 
     # Set blackpoint to min(target_black, blackpoint).
     black = np.minimum(target_black, blackpoint)
